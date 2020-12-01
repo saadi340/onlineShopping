918931ba0096c728ef61c6a792311f18931a63fb
Saadi340 <saad.shabbir1435@gmail.com>
2020-12-01 12:24:29 -0800

    Updates

---
+++ precache-manifest.11591377b546d8643abed2188d7ce868.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8653b5bc62dd3fdabe3901aa93fd15d",
+    "url": "/onlineShopping/index.html"
+  },
+  {
+    "revision": "33cbe8594a92809e51a7",
+    "url": "/onlineShopping/static/css/2.b16e441d.chunk.css"
+  },
+  {
+    "revision": "bdb28df7746a563fb03f",
+    "url": "/onlineShopping/static/css/main.4fc66313.chunk.css"
+  },
+  {
+    "revision": "33cbe8594a92809e51a7",
+    "url": "/onlineShopping/static/js/2.079427ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/onlineShopping/static/js/2.079427ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb28df7746a563fb03f",
+    "url": "/onlineShopping/static/js/main.aadae4d3.chunk.js"
+  },
+  {
+    "revision": "d7592bfc223c0272e0e4",
+    "url": "/onlineShopping/static/js/runtime-main.28978bb3.js"
+  },
+  {
+    "revision": "0210e597f574b4bd3f751ca6b3629f1c",
+    "url": "/onlineShopping/static/media/bbb.0210e597.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/onlineShopping/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/onlineShopping/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/onlineShopping/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/onlineShopping/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/onlineShopping/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/onlineShopping/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/onlineShopping/static/media/watch.b4e58e91.jpg"
+  }
+]);
